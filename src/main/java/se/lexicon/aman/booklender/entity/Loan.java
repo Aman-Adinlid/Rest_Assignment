2c32322d295a47e90f21c6ad5f30d8b9b9094bbf
aman adinlid <95pinkpanda@gmail.com>
2021-04-23 15:58:55 +0200

    ##edited loan class,all tables are created in the database and no exceptions are thrown##

--- src/main/java/se/lexicon/aman/booklender/entity/Loan.java
+++ src/main/java/se/lexicon/aman/booklender/entity/Loan.java
@@ -11,8 +11,15 @@ public class Loan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long loanId;
+
     @ManyToOne
+    @JoinColumn(name = "library_user_user_id")
     private LibraryUser loanTaker;
+
+    @ManyToOne
+    @JoinColumn(name = "book_book_id")
+    private Book book;
     private LocalDate loanDate;
+   @Column(name = "_terminated")
     private boolean terminated;
 }
