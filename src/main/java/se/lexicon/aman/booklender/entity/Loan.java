44cf3368d0e9dceaa8245b91ffeb86f6d1623fd7
aman adinlid <95pinkpanda@gmail.com>
2021-04-23 14:21:22 +0200

    ##Implmented Loan class in entity##

--- src/main/java/se/lexicon/aman/booklender/entity/Loan.java
+++ src/main/java/se/lexicon/aman/booklender/entity/Loan.java
@@ -1,4 +1,18 @@
 package se.lexicon.aman.booklender.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long loanId;
+    @ManyToOne
+    private LibraryUser loanTaker;
+    private LocalDate loanDate;
+    private boolean terminated;
 }
