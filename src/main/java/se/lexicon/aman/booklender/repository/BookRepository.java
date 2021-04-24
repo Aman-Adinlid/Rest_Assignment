2653752dba3eb34224f390841d29e08fdf1dc1e1
aman adinlid <95pinkpanda@gmail.com>
2021-04-24 14:11:30 +0200

    ##Implmented book repository methods##

--- src/main/java/se/lexicon/aman/booklender/repository/BookRepository.java
+++ src/main/java/se/lexicon/aman/booklender/repository/BookRepository.java
@@ -1,4 +1,16 @@
 package se.lexicon.aman.booklender.repository;
 
-public interface BookRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.aman.booklender.entity.Book;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+    List<Book> findByReserved(boolean status);
+
+    List<Book> findByAvailable(boolean status);
+
+    List<Book> findByTitle(String title);
+
 }
