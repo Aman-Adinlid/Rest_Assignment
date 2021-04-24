74f184f77ad44dc278424d75b1ab370100398c18
aman adinlid <95pinkpanda@gmail.com>
2021-04-24 19:14:04 +0200

    ##Implmented bookDto##

--- src/main/java/se/lexicon/aman/booklender/dto/BookDto.java
+++ src/main/java/se/lexicon/aman/booklender/dto/BookDto.java
@@ -1,4 +1,16 @@
 package se.lexicon.aman.booklender.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
 public class BookDto {
+
+    private int bookId;
+    private String title;
+    private boolean available;
+    private boolean reserved;
+    private int maxLoanDays;
+    private BigDecimal finePerDay;
+    private String description;
 }
