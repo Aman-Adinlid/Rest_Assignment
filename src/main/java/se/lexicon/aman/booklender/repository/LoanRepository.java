c622f6fb900194c47262eac0978491f5fc0754fa
aman adinlid <95pinkpanda@gmail.com>
2021-04-24 14:50:20 +0200

    ##Implmented loan repository methods##

--- src/main/java/se/lexicon/aman/booklender/repository/LoanRepository.java
+++ src/main/java/se/lexicon/aman/booklender/repository/LoanRepository.java
@@ -1,4 +1,16 @@
 package se.lexicon.aman.booklender.repository;
 
-public interface LoanRepository {
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.aman.booklender.entity.Loan;
+
+import java.util.List;
+
+public interface LoanRepository extends CrudRepository<Loan, Long> {
+
+    List<Loan> findByUserId(int userId);
+
+    List<Loan> findByBookId(int bookId);
+
+    List<Loan> findByTerminated(boolean status);
+
 }
