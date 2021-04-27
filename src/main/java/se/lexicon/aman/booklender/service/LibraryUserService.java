5a9835c9bfbeabdcc5471a5c6bb2b7f469ceac42
aman adinlid <95pinkpanda@gmail.com>
2021-04-28 00:56:46 +0200

    ##Edited library user service methods##

--- src/main/java/se/lexicon/aman/booklender/service/LibraryUserService.java
+++ src/main/java/se/lexicon/aman/booklender/service/LibraryUserService.java
@@ -1,11 +1,12 @@
 package se.lexicon.aman.booklender.service;
 
+import org.springframework.dao.DataAccessException;
 import se.lexicon.aman.booklender.dto.LibraryUserDto;
 
 import java.util.List;
 
 public interface LibraryUserService {
-    LibraryUserDto findById(int userId);
+    LibraryUserDto findById(int userId) throws DataAccessException;
 
     LibraryUserDto findByEmail(String email);
 
@@ -13,9 +14,9 @@ public interface LibraryUserService {
 
     LibraryUserDto create(LibraryUserDto libraryUserDto);
 
-    LibraryUserDto update(LibraryUserDto libraryUserDto);
+    LibraryUserDto update(LibraryUserDto libraryUserDto) throws DataAccessException;
 
-    boolean delete(int userId);
+    boolean delete(int userId) throws DataAccessException;
 
 
 }
