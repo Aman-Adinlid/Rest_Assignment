21ff3274a1d5d5c3215b51648b5dd6b1e32dc134
aman adinlid <95pinkpanda@gmail.com>
2021-04-29 01:00:17 +0200

    ##Edited exception##

--- src/main/java/se/lexicon/aman/booklender/exception/DataNotFoundException.java
+++ src/main/java/se/lexicon/aman/booklender/exception/DataNotFoundException.java
@@ -2,7 +2,6 @@ package se.lexicon.aman.booklender.exception;
 
 public class DataNotFoundException extends Exception {
 
-
     public DataNotFoundException(String message) {
         super(message);
     }
